89fe294f45f80c162a2edd9f812e11d5ffc2da51
Jarrod West <jnw.dev@gmail.com>
2024-03-24 01:50:51 +1100

    List observer, opacity for all

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,47 @@
-import { debug, info, error } from "./utils/utils";
+import { parsePath, debug, info, error } from "./utils/utils";
 
+// Classes
 const ROW_LISTINGS_CLASS_NAME = "user-ad-row-new-design";
 const GALLERY_LISTINGS_CLASS_NAME = "user-ad-square-new-design";
+const DISPLAY_RADIO_BUTTON_CLASS_NAME = "radio-set__option srp-display-options__view";
+
+// Settings
 const OPACITY = "0.5";
 
-const getListings = (view: string): HTMLCollectionOf<HTMLElement> => {
+const getListings = (view: string | undefined): HTMLCollectionOf<HTMLElement> => {
   const className = view === "gallery" ? GALLERY_LISTINGS_CLASS_NAME : ROW_LISTINGS_CLASS_NAME;
 
   return document.getElementsByClassName(className) as HTMLCollectionOf<HTMLElement>;
 }
 
+const mutationCallback: MutationCallback = (mutationList: MutationRecord[], _observer: MutationObserver) => {
+  // Rerun when mutation occurs
+  debug(`Mutations occurred: ${mutationList.length}`);
+  main();
+};
+
+
+const createDisplayChangeObserver = () => {
+  // Listen for changes to the radio button that chooses between "list" and "grid"
+  const targetNode = document.getElementsByClassName(DISPLAY_RADIO_BUTTON_CLASS_NAME)?.[0];
+  if (targetNode) {
+    debug("Setting observer");
+    const config: MutationObserverInit = { attributes: true, subtree: true };
+    const observer = new MutationObserver(mutationCallback);
+    observer.observe(targetNode, config);
+  } else {
+    error("Failed to set observer");
+  }
+}
+
 const main = () => {
   debug("Main");
+  // Leave this in to make it clear the extension loaded
   document.body.style.border = "5px solid red";
 
-  const listings = getListings("gallery");
+  const urlComponents = parsePath(document.URL);
+
+  const listings = getListings(urlComponents.view);
 
   debug(`Found ${listings.length} listings`);
 
@@ -24,26 +51,7 @@ const main = () => {
   }
 }
 
-const mutationCallback = (mutationList: any, observer: any) => {
-  for (const mutation of mutationList) {
-    // if (mutation.type === "childList") {
-    //   console.log("A child node has been added or removed.");
-    // } else if (mutation.type === "attributes") {
-    //   console.log(`The ${mutation.attributeName} attribute was modified.`);
-    // }
-    debug(`Mutation of type: ${mutation.type}`);
-  }
-};
-
-const targetNode = document.getElementsByClassName("radio-set__option srp-display-options__view");
-if (targetNode) {
-  debug("Setting observer");
-  const config = { attributes: true, childList: true, subtree: true };
-  const observer = new MutationObserver(mutationCallback);
-  observer.observe(targetNode[0], config);
-} else {
-  error("Failed to set observer");
-}
 
 info("Loaded");
+createDisplayChangeObserver();
 main();
