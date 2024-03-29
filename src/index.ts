4def1a78d3fa0622499b72d9890d43f5a2f1b9a0
Jarrod West <jnw.dev@gmail.com>
2024-03-29 18:47:25 +1100

    Slider changing transparency

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,30 @@
 import { debug, info, error } from "./utils/helpers";
 import { parsePath, getListings, parseListing } from "./utils/parsers";
 import { filterListing } from "./filter";
+import { SettingsMessage } from "./types";
 
 // Classes
 const MAIN_RESULTS_CLASS_NAME = "search-results-page__user-ad-collection";
 
 // Settings
-const OPACITY = "0.5";
+const DEFAULT_SETTINGS = {
+  transparency: 70,
+}
+
+const setInitialSettings = async () => {
+  const currentStorage = await browser.storage.sync.get();
+  // Object.entries(DEFAULT_SETTINGS).forEach(([key, value]) => {
+  //   if (!currentStorage[key]) {
+  //     await browser.storage.sync.set({key: value});
+  //   }
+  // });
+
+  // Object.keys(DEFAULT_SETTINGS).filter(key => !(currentStorage.keys().includes(key)))
+  //   .reduce( (res, key) => (res[key] = DEFAULT_SETTINGS[key], res), {} );
+
+  const unset = Object.fromEntries(Object.entries(DEFAULT_SETTINGS).filter(([key, _value]) => !currentStorage.keys().includes(key)));
+  await browser.storage.sync.set(unset);
+}
 
 const mutationCallback: MutationCallback = (mutationList: MutationRecord[], _observer: MutationObserver) => {
   // Rerun when mutation occurs
@@ -48,23 +66,55 @@ const main = async () => {
   const listingsNode = getListings(urlComponents.view);
 
   debug(`Found ${listingsNode.length} listings`);
+  const transparency: number = parseInt((await browser.storage.sync.get("transparency")).transparency);
 
   for (const listingNode of listingsNode) {
     const listing = parseListing(urlComponents.view, listingNode);
     if (filterListing(urlComponents.searchQuery, listing)) {
-      listingNode.style.opacity = OPACITY;
+      // listingNode.style.opacity = OPACITY;
+      listingNode.style.opacity = ((100 - transparency) / 100).toString();
     }
   }
 }
 
-browser.runtime.onMessage.addListener((request) => {
-  debug(`Message received: ${request.message}`);
+const onSettingsMessage = async (message: SettingsMessage) => {
+  debug(`Message received: ${JSON.stringify(message)}`);
+  await browser.storage.sync.set(message);
 
-  return Promise.resolve({response: "Response from content"});
-})
+  main();
+
+  return {response: "Response from content"};
+}
+
+// browser.runtime.onMessage.addListener(async (message) => {
+//   debug(`Message received: ${JSON.stringify(message)}`);
+
+//   Object.entries(message).forEach(([key, value]) => {
+    
+//   });
+
+//   await browser.storage.sync.set({})
+
+//   // return Promise.resolve({response: "Response from content"});
+//   return {response: "Response from content"};
+// });
+
+// export const saveOptions = async (event: Event) => {
+//   event.preventDefault();
+
+//   const element = document.querySelector("#color") as HTMLInputElement;
+
+//   if (element) {
+//     await browser.storage.sync.set({
+//       color: element.value,
+//     });
+//   }
+// }
 
 
 info("Loaded");
 createDisplayChangeObserver();
+setInitialSettings();
+browser.runtime.onMessage.addListener(onSettingsMessage);
 // createToolbarButton();
 // main(); // TODO: Still necessary, or does the initial load get triggered as a mutation?
--- src/popup.ts
+++ src/popup.ts
@@ -1,29 +1,65 @@
 import { debug, error } from "./utils/helpers";
 
-document.addEventListener("click", async (event: Event) => {
+
+const getEventTarget = (event: Event): HTMLElement | null => {
   if (!event?.target) {
-    return;
+    return null;
   }
 
-  const target = event.target as HTMLElement;
+  return event.target as HTMLElement;
+}
 
-  if (!target.classList.contains("page-choice")) {
-    return;
-  }
-
-  // const chosenPage = `https://${target.textContent}`;
-  // browser.tabs.create({
-  //   url: chosenPage,
-  // });
-
+const sendMessage = async (message: any): Promise<void> => {
   try {
     const tabs = await browser.tabs.query({currentWindow: true, active: true});
 
     for (const tab of tabs) {
-      const response = await browser.tabs.sendMessage(tab.id as number, {message: "a message"});
+      const response = await browser.tabs.sendMessage(tab.id as number, message);
       debug(`Message sent, response: ${response.response}`);
     }
   } catch (err) {
     error(`Error sending message: ${err}`);
   }
-});
+}
+
+// document.addEventListener("click", async (event: Event) => {
+//   if (!event?.target) {
+//     return;
+//   }
+
+//   const target = event.target as HTMLElement;
+
+//   // if (!target.classList.contains("page-choice")) {
+//   //   return;
+//   // }
+
+//   // const chosenPage = `https://${target.textContent}`;
+//   // browser.tabs.create({
+//   //   url: chosenPage,
+//   // });
+
+//   try {
+//     const tabs = await browser.tabs.query({currentWindow: true, active: true});
+
+//     for (const tab of tabs) {
+//       const response = await browser.tabs.sendMessage(tab.id as number, {message: "a message"});
+//       debug(`Message sent, response: ${response.response}`);
+//     }
+//   } catch (err) {
+//     error(`Error sending message: ${err}`);
+//   }
+// });
+
+
+const slider = document.getElementById("transparency-slider") as HTMLInputElement;
+if (slider) {
+  slider.addEventListener("change", async (event: Event) => {
+    const target = getEventTarget(event);
+
+    // if (target?.id === "transparency-slider") {
+    //   // Send the message
+    //   await sendMessage({transparency: target.nodeValue});
+    // }
+    await sendMessage({transparency: slider.value});
+  });
+}
